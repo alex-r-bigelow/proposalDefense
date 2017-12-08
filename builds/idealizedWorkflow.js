ef1c9fb5d75245b45383cc614a806b3579552a12
Alex Bigelow <alex.r.bigelow@gmail.com>
2017-12-08 10:19:50 -0700

    Initial support for bridge model circle builds

---
+++ builds/idealizedWorkflow.js
@@ -0,0 +1,294 @@
+/* globals addBuild */
+
+addBuild('idealizedWorkflow', [
+  {
+    circles: {
+      automated: {
+        x: 0.25,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'lightgrey',
+          northwest: 'lightgrey',
+          north: 'lightgrey',
+          northeast: 'lightgrey',
+          northeasteast: 'lightgrey',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      },
+      drawing: {
+        x: 0.25,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'lightgrey',
+          northwest: 'lightgrey',
+          north: 'lightgrey',
+          northeast: 'lightgrey',
+          northeasteast: 'lightgrey',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      }
+    }
+  },
+  {
+    circles: {
+      automated: {
+        x: 0.25,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'lightgrey',
+          northwest: 'lightgrey',
+          north: 'lightgrey',
+          northeast: 'lightgrey',
+          northeasteast: 'lightgrey',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      },
+      drawing: {
+        x: 0.75,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'lightgrey',
+          northwest: 'lightgrey',
+          north: 'lightgrey',
+          northeast: 'lightgrey',
+          northeasteast: 'lightgrey',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      }
+    }
+  },
+  {
+    circles: {
+      automated: {
+        x: 0.25,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'lightgrey',
+          northwest: 'lightgrey',
+          north: 'lightgrey',
+          northeast: 'lightgrey',
+          northeasteast: 'lightgrey',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      },
+      drawing: {
+        x: 0.75,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'lightgrey',
+          northwest: 'lightgrey',
+          north: 'mediumGreen',
+          northeast: 'mediumGreen',
+          northeasteast: 'mediumGreen',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      },
+      newAutomated: {
+        x: 0.75,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'lightgrey',
+          northwest: 'lightgrey',
+          north: 'mediumGreen',
+          northeast: 'mediumGreen',
+          northeasteast: 'mediumGreen',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      }
+    }
+  },
+  {
+    circles: {
+      drawing: {
+        x: 0.75,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'lightgrey',
+          northwest: 'lightgrey',
+          north: 'mediumGreen',
+          northeast: 'mediumGreen',
+          northeasteast: 'mediumGreen',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      },
+      newAutomated: {
+        x: 0.25,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'lightgrey',
+          northwest: 'lightgrey',
+          north: 'mediumGreen',
+          northeast: 'mediumGreen',
+          northeasteast: 'mediumGreen',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      }
+    }
+  },
+  {
+    circles: {
+      drawing: {
+        x: 0.75,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'lightgrey',
+          northwest: 'lightgrey',
+          north: 'mediumGreen',
+          northeast: 'mediumGreen',
+          northeasteast: 'mediumGreen',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      },
+      newAutomated: {
+        x: 0.25,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'mediumBlue',
+          northwest: 'mediumBlue',
+          north: 'teal',
+          northeast: 'mediumGreen',
+          northeasteast: 'mediumGreen',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      },
+      newDrawing: {
+        x: 0.25,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'mediumBlue',
+          northwest: 'mediumBlue',
+          north: 'teal',
+          northeast: 'mediumGreen',
+          northeasteast: 'mediumGreen',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      }
+    }
+  },
+  {
+    circles: {
+      newAutomated: {
+        x: 0.25,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'mediumBlue',
+          northwest: 'mediumBlue',
+          north: 'teal',
+          northeast: 'mediumGreen',
+          northeasteast: 'mediumGreen',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      },
+      newDrawing: {
+        x: 0.75,
+        y: 0.5,
+        scale: 3,
+        colors: {
+          west: 'lightgrey',
+          northwestwest: 'mediumBlue',
+          northwest: 'mediumBlue',
+          north: 'teal',
+          northeast: 'mediumGreen',
+          northeasteast: 'mediumGreen',
+          east: 'lightgrey',
+          southwest: 'lightgrey',
+          southwestwest: 'lightgrey',
+          southeast: 'lightgrey',
+          southeasteast: 'lightgrey',
+          south: 'lightgrey'
+        }
+      }
+    }
+  }
+]);
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+/* globals Reveal, hljs, allBuilds, handleStep */
+
 Reveal.initialize({
-  width: "100%",
-  height: "100%",
+  width: 1920,
+  height: 1080,
   margin: 0,
-  minScale: 1,
-  maxScale: 1,
+  // minScale: 1,
+  // maxScale: 1,
   controls: false,
   slideNumber: true,
   transition: 'fade',
@@ -11,7 +13,26 @@ Reveal.initialize({
   dependencies: [
     { src: 'plugin/markdown/marked.js' },
     { src: 'plugin/markdown/markdown.js' },
-    { src: 'plugin/notes/notes.js', async: true },
-    { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
-  ]
+    { src: 'plugin/notes/notes.js',
+      async: true
+    },
+    { src: 'plugin/highlight/highlight.js',
+      async: true,
+      callback: function () {
+        hljs.initHighlightingOnLoad();
+      }
+    }
+  ],
+  history: true,
+  center: false
+});
+
+Reveal.addEventListener('slidechanged', function (event) {
+  let eventId = event.currentSlide.getAttribute('id');
+  if (allBuilds[eventId]) {
+    let build = allBuilds[eventId];
+    Reveal.addEventListener('fragmentshown', build.callback);
+    // Handle the first step immediately
+    handleStep(build.sectionId, 0);
+  }
 });
